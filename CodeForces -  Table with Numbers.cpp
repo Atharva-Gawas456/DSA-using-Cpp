d02485f6fee55780c3783b579d990a74fa508d8b
Atharva-Gawas456 <atharva.gawas182@gmail.com>
2026-01-25 23:34:14 +0530

    BreakDay

---
+++ CodeForces -  Table with Numbers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, h, l;
+        cin >> n >> h >> l;
+
+        int R = 0, C = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            if (x <= h) R++;
+            if (x <= l) C++;
+        }
+
+        cout << min(R, C) << '\n';
+    }
+
+    return 0;
+}
